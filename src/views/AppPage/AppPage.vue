d374236494e85cd1bf085543b85e182a0121ff67
Lauren <desica.org@gmail.com>
2021-01-16 16:20:14 +0000

    Start work on app

--- src/views/AppPage/AppPage.vue
+++ src/views/AppPage/AppPage.vue
@@ -1,7 +1,54 @@
 <template>
-    <section></section>
+  <div class="sample">
+    <div class="row">
+      <cv-tile :light="light">
+        <h1>Hello</h1>
+        <p>This is some tile content</p>
+      </cv-tile>
+      <cv-tile :light="light">
+        <h1>Hello</h1>
+        <p>This is some tile content</p>
+      </cv-tile>
+    </div>
+  </div>
 </template>
+
+<script>
+import 'carbon-components/css/carbon-components.css';
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      yourName: '',
+      visible: false
+    };
+  },
+  methods: {
+    onClick() {
+      this.visible = true;
+    },
+    modalClosed() {
+      this.visible = false;
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 @import '../../styles/carbon-utils';
+
+.sample {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 600px;
+  margin: 5% auto;
+  height: 100%;
+}
+
+.cv-text-input {
+  margin: 30px 0;
+}
 </style>
